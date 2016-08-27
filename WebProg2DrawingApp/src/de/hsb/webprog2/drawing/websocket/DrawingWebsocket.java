64e70b55d0758a23e70a21ebd43083b3d06a706c
RoseFlunder <stephan.maevers@gmail.com>
2016-08-27 15:34:57 +0200

    new websocket

---
+++ WebProg2DrawingApp/src/de/hsb/webprog2/drawing/websocket/DrawingWebsocket.java
@@ -0,0 +1,39 @@
+package de.hsb.webprog2.drawing.websocket;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.websocket.EndpointConfig;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint(value = "/websocket/drawing")
+public class DrawingWebsocket {
+
+	private static Set<Session> clients = Collections.synchronizedSet(new HashSet<>());
+	
+	@OnOpen
+	public void open(Session session, EndpointConfig config) {
+		clients.add(session);
+	}
+	
+	@OnClose
+	public void close(Session session){
+		clients.remove(session);
+	}
+	
+	@OnMessage
+	public void onMessage(Session session, String message){
+		try {
+			session.getBasicRemote().sendText("danke, beste");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
